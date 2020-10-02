1e643979d0ed97708b461da18b336a6f60e1a926
fabricio <fabriciojf@gmail.com>
2020-10-01 21:16:49 -0300

    Starting the project

---
+++ src/fabriciojf/com/BinaryTree.java
@@ -0,0 +1,83 @@
+package fabriciojf.com;
+
+import java.util.*;
+
+/**
+ * Code Challenge 
+ * @since 2020/09/30
+ * @version 0.1
+ * @author Fabricio S Costa fabriciojf@gmail.com
+ */
+public class BinaryTree {
+
+    public int run(Node leftNode, Node righNode, String rootOperator)  throws Exception  {
+        
+        switch (rootOperator) {
+            case "+":
+                return leftNode.total() + righNode.total();
+            
+            case "-": 
+                return leftNode.total() - righNode.total();
+
+            case "*":
+                return leftNode.total() * righNode.total();
+            
+            case "/": 
+                return leftNode.total() / righNode.total();
+            
+            default:
+                throw new Exception("Operator not implemented");
+        }
+    } 
+
+
+    public static void main(String[] args) {
+        
+        Node node1 = new Node(3, 2, "+");
+        Node node2 = new Node(4, 5, "+");
+    
+        try {
+            int value = new BinaryTree().run(node1, node2, "9");
+            System.out.println(value);
+        } catch(Exception e) {
+            System.out.println("Problems calculating the Tree, please verify the code!");            
+        }
+
+    }
+
+}
+
+class Node {
+
+    private int leftValue;
+    private int rightValue;
+    private String operator;
+
+    public Node(int leftValue, int rightValue, String operator) {
+        this.leftValue = leftValue;
+        this.rightValue = rightValue;
+        this.operator = operator;
+    }
+
+    /**
+     * Compute the node
+     */
+    public int total() throws Exception {
+        switch (operator) {
+            case "+":
+                return leftValue + rightValue;
+            
+            case "-": 
+                return leftValue - rightValue;
+
+            case "*":
+                return leftValue * rightValue;
+            
+            case "/": 
+                return leftValue / rightValue;
+            
+            default:
+                throw new Exception("Operator not implemented");
+        }
+    }
+}
